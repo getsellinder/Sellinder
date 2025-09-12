70bc8d596c8c189a6611f9592968653afb422f83
getsellinder <getsellinder@gmail.com>
2025-09-12 10:55:53 +0530

    contact-done

---
+++ components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div className='text-center'>
+            <h1>Contact Us</h1>
+            <span>We'd love to hear from you. Send a message and we’ll respond soon.</span>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,15 +1,113 @@
-import Footer from "../../components/Footer"
-import Header from "../../components/Header"
-
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import { IoLocationOutline } from "react-icons/io5";
+import { HiOutlineMail } from "react-icons/hi";
+import { IoCallOutline } from "react-icons/io5";
 
 const Contact = () => {
   return (
-    <div>
-           <Header/>
-           <h1>Contact</h1>
-      <Footer/>
-    </div>
-  )
-}
+    <div className="bg-white min-h-screen ">
+      <Header />
+      <h2 class="text-5xl font-bold text-gray-700 mb-2 text-center my-3">
+        Contact Us
+      </h2>
+      <p class="  font-medium text-center text-gray-500 dark:text-gray-500 sm:text-xl mt-5">
+        We'd love to hear from you. Send a message and we’ll respond soon.
+      </p>
 
-export default Contact
+      <div className="bg-white flex items-center justify-center p-6">
+        <div className="max-w-7xl w-full bg-white shadow-lg rounded-2xl p-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+          {/* Left side - Form */}
+          <div>
+            <form className="space-y-4 p-10 border rounded-md">
+              {/* Name */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Your name"
+                  className="w-full px-4 py-3 border rounded-lg 
+                  focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                />
+              </div>
+
+              {/* Email */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Email or Phone number
+                </label>
+                <input
+                  type="text"
+                  placeholder="you@example.com or +1 555 123 4567"
+                  className="w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                />
+              </div>
+
+              {/* Message */}
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Message
+                </label>
+                <textarea
+                  rows="4"
+                  placeholder="How can we help?"
+                  className="w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                ></textarea>
+              </div>
+
+              {/* Button */}
+              <button
+                type="submit"
+                className="w-full bg-gradient-to-r from-blue-500 to-blue-600 text-white py-2 rounded-lg font-semibold hover:from-blue-600 hover:to-blue-700 transition"
+              >
+                Send Message
+              </button>
+            </form>
+          </div>
+
+          {/* Right side - Info */}
+          <div className="space-y-6">
+            <div className="p-4 border rounded-lg ">
+             <div className="flex items-center gap-3 mb-3">
+          <IoLocationOutline className="text-blue-600 text-2xl" />
+          <span className="font-semibold text-gray-800">Address</span>
+        </div>
+
+              <p className="text-gray-600">
+                Neonflake Enterprises OPC Pvt Ltd <br />
+                303, 3rd Floor, Meridian Plaza <br />
+                Greenlands, Ameerpet <br />
+                Hyderabad, India 500016
+              </p>
+            </div>
+
+            <div className="p-4 border rounded-lg">
+                <div className="flex items-center gap-3 mb-3">
+          <HiOutlineMail className="text-blue-600 text-2xl" />
+          <span className="font-semibold text-gray-800">Email</span>
+        </div>
+              
+            
+              <p className="text-gray-600">hello@smellika.com</p>
+            </div>
+
+            <div className="p-4 border rounded-lg">
+                    <div className="flex items-center gap-3 mb-3">
+          <IoCallOutline className="text-blue-600 text-2xl" />
+          <span className="font-semibold text-gray-800">Contact</span>
+        </div>
+          
+              <p className="text-gray-600">+91 8977002747</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
